334e4d87dc343caac002f594c99e11bf25abdceb
rhenandias <diasrhenan@gmail.com>
2022-11-27 11:44:25 -0300

    Estilizando lista de clientes

--- src/screens/Create/index.tsx
+++ src/screens/Create/index.tsx
@@ -1,43 +1,126 @@
-import React, { useContext, useEffect } from "react";
-import { Box, Text } from "native-base";
+import React, { useState, useContext, useEffect } from "react";
+import { Box, Button, Center, FormControl, Input, Select, Text, VStack } from "native-base";
 import { DatabaseContext, DatabaseContextProps } from "../../../database/context";
 import { Client } from "../../../database/entities/client";
 
 export function Create() {
   const { database } = useContext(DatabaseContext) as DatabaseContextProps;
 
-  useEffect(() => {
-    async function load() {
-      const client = new Client();
+  const [nome, setNome] = useState("");
+  const [razaoSocial, setRazaoSocial] = useState("");
+  const [cpfCnpj, setCpfCnpj] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [municipio, setMunicipo] = useState("");
+  const [uf, setUf] = useState("");
 
-      client.nome = "Dev Fazer";
-      client.razaoSocial = "Dev Fazer LTDA.";
-      client.cpfCnpj = "123";
-      client.endereco = "IFSP Guarulhos";
-      client.municipio = "Guarulhos";
-      client.uf = "São Paulo";
+  async function handleSubmit() {
+    const clientRepository = database.getRepository(Client);
 
-      const clientRepository = database.getRepository(Client);
+    const client = clientRepository.create({
+      nome,
+      razaoSocial,
+      cpfCnpj,
+      endereco,
+      municipio,
+      uf,
+    });
 
-      await clientRepository.save(client);
-
-      console.log("Sucesso na criação do cliente:", client.id);
-
-      const loadedClient = await clientRepository.findOne({
-        where: { id: client.id },
-      });
-
-      if (loadedClient) {
-        console.log("Cliente carregado: ", loadedClient);
-      }
-    }
-
-    load();
-  }, []);
+    await clientRepository.save(client);
+  }
 
   return (
-    <Box>
-      <Text>Create</Text>
-    </Box>
+    <>
+      <Center my={6}>
+        <Text fontWeight={"bold"} color={"gray.600"}>
+          Cadastrar Cliente
+        </Text>
+      </Center>
+      <Center>
+        <VStack space={3} w="80%">
+          <Box alignItems="center">
+            <Box w="100%" maxWidth="300px">
+              <FormControl>
+                <Input
+                  value={nome}
+                  onChangeText={(text) => setNome(text)}
+                  type="text"
+                  placeholder="Nome"
+                />
+              </FormControl>
+              <FormControl mt={3}>
+                <Input
+                  value={razaoSocial}
+                  onChangeText={(text) => setRazaoSocial(text)}
+                  type="text"
+                  placeholder="Razão Social"
+                />
+              </FormControl>
+              <FormControl mt={3}>
+                <Input
+                  value={cpfCnpj}
+                  onChangeText={(text) => setCpfCnpj(text)}
+                  type="text"
+                  placeholder="CPF / CNPJ"
+                />
+              </FormControl>
+              <FormControl mt={3}>
+                <Input
+                  value={endereco}
+                  onChangeText={(text) => setEndereco(text)}
+                  type="text"
+                  placeholder="Endereço"
+                />
+              </FormControl>
+              <FormControl mt={3}>
+                <Input
+                  value={municipio}
+                  onChangeText={(text) => setMunicipo(text)}
+                  type="text"
+                  placeholder="Município"
+                />
+              </FormControl>
+              <FormControl mt={3}>
+                <Select onValueChange={(value) => setUf(value)} placeholder="Selecione o Estado">
+                  <Select.Item value="AC" label="Acre" />
+                  <Select.Item value="AL" label="Alagoas" />
+                  <Select.Item value="AP" label="Amapá" />
+                  <Select.Item value="AM" label="Amazonas" />
+                  <Select.Item value="BA" label="Bahia" />
+                  <Select.Item value="CE" label="Ceará" />
+                  <Select.Item value="DF" label="Distrito Federal" />
+                  <Select.Item value="ES" label="Espírito Santo" />
+                  <Select.Item value="GO" label="Goiás" />
+                  <Select.Item value="MA" label="Maranhão" />
+                  <Select.Item value="MT" label="Mato Grosso" />
+                  <Select.Item value="MS" label="Mato Grosso do Sul" />
+                  <Select.Item value="MG" label="Minas Gerais" />
+                  <Select.Item value="PA" label="Pará" />
+                  <Select.Item value="PB" label="Paraíba" />
+                  <Select.Item value="PR" label="Paraná" />
+                  <Select.Item value="PE" label="Pernambuco" />
+                  <Select.Item value="PI" label="Piauí" />
+                  <Select.Item value="RJ" label="Rio de Janeiro" />
+                  <Select.Item value="RN" label="Rio  Grande do Norte" />
+                  <Select.Item value="RS" label="Rio Grande do Sul" />
+                  <Select.Item value="RO" label="Rondônia" />
+                  <Select.Item value="RR" label="Roraima" />
+                  <Select.Item value="SC" label="Santa Catarina" />
+                  <Select.Item value="SP" label="São Paulo" />
+                  <Select.Item value="SE" label="Sergipe" />
+                  <Select.Item value="TO" label="Tocantins" />
+                  <Select.Item value="EX" label="Estrangeiro" />
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+
+          <Button colorScheme="success" w="100%" mt={3} onPress={handleSubmit}>
+            <Text textTransform="uppercase" color="white">
+              Cadastrar
+            </Text>
+          </Button>
+        </VStack>
+      </Center>
+    </>
   );
 }
--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,9 +1,120 @@
-import { Box, Text } from "native-base";
+import React, { useContext, useEffect, useState } from "react";
+import { Box, Center, Flex, Icon, IconButton, Text, VStack } from "native-base";
+
+import { DatabaseContext, DatabaseContextProps } from "../../../database/context";
+import { Client } from "../../../database/entities/client";
+
+import { Entypo } from "@expo/vector-icons";
+
+function ClientCard(client: Client) {
+  return (
+    <VStack padding={4} space={2} rounded="lg" bgColor="white">
+      <Flex flexDirection={"row"} justify={"space-between"}>
+        <Text fontWeight={"bold"}>Nome:</Text>
+        <Text>{client.nome}</Text>
+      </Flex>
+      <Flex flexDirection={"row"} justify={"space-between"}>
+        <Text fontWeight={"bold"}>Razão Social:</Text>
+        <Text>{client.razaoSocial}</Text>
+      </Flex>
+      <Flex flexDirection={"row"} justify={"space-between"}>
+        <Text fontWeight={"bold"}>CPF / CNPJ:</Text>
+        <Text>{client.cpfCnpj}</Text>
+      </Flex>
+      <Flex flexDirection={"row"} justify={"space-between"}>
+        <Text fontWeight={"bold"}>Endereço:</Text>
+        <Text>
+          {client.endereco} - {client.uf}
+        </Text>
+      </Flex>
+      <Flex flexDirection={"row"} justify={"flex-end"}>
+        <IconButton
+          icon={<Icon as={Entypo} name="emoji-happy" />}
+          borderRadius="full"
+          _icon={{
+            color: "orange.500",
+            size: "md",
+          }}
+          _hover={{
+            bg: "orange.600:alpha.20",
+          }}
+          _pressed={{
+            bg: "orange.600:alpha.20",
+            _icon: {
+              name: "emoji-flirt",
+            },
+            _ios: {
+              _icon: {
+                size: "2xl",
+              },
+            },
+          }}
+          _ios={{
+            _icon: {
+              size: "2xl",
+            },
+          }}
+        />
+      </Flex>
+    </VStack>
+  );
+}
 
 export function List() {
+  const { database } = useContext(DatabaseContext) as DatabaseContextProps;
+
+  const [clients, setClients] = useState<Client[]>();
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function load() {
+      const clientRespository = database.getRepository(Client);
+
+      const clients = await clientRespository.find();
+
+      setClients(clients);
+      setLoaded(true);
+    }
+
+    load();
+  }, []);
+
   return (
-    <Box>
-      <Text>List</Text>
-    </Box>
+    <>
+      <Center my={6}>
+        <Text fontWeight={"bold"} color={"gray.600"}>
+          Listar Clientes
+        </Text>
+      </Center>
+
+      <Center mt={4}>
+        <VStack w={"90%"}>
+          {loaded && (
+            <>
+              {clients?.map((client) => (
+                <ClientCard
+                  key={client.id}
+                  id={client.id}
+                  nome={client.nome}
+                  razaoSocial={client.razaoSocial}
+                  cpfCnpj={client.cpfCnpj}
+                  endereco={client.endereco}
+                  municipio={client.municipio}
+                  uf={client.uf}
+                />
+              ))}
+            </>
+          )}
+        </VStack>
+      </Center>
+
+      {!loaded && (
+        <>
+          <Center>
+            <Text>Carregando</Text>
+          </Center>
+        </>
+      )}
+    </>
   );
 }
